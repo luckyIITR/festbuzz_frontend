3ffb8ff8619f88860cb9f423a673e35a326daf28
ItsUjjwalGoel <ujjwalgoel104@gmail.com>
2025-09-03 19:22:05 +0530

    explore fests ui

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const { user, logout } = useAuth();
   const { canManageUsers } = usePermissions();
  
-  function handleLogout()  {
+  function handleLogout() {
     logout();
     window.location.reload();
   }
